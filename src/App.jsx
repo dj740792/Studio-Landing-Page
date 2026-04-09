961bd7993627ca14bfa4d9174211d97bf5ba3ceb
Dhruv <dj740792@gmail.com>
2026-04-09 22:22:10 +0530

    added functionality to the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 import React from "react";
+import Navbar from "./components/Navbar"; // Make sure this path is right
+import Kern from "./pages/Kern";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Works from "./pages/Works";
 import Testimonial from "./pages/Testimonial";
-import Kern from "./pages/Kern";
-import Stats from "./components/Stats";
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <Kern />
-      <About />
-      <Services />
-      <Works />
-      <Testimonial />
+    <div className="relative bg-[#f1f1f1]">
+      <div id="home">
+        <Kern />
+      </div>
+
+      <div id="about">
+        <About />
+      </div>
+
+      <div id="services">
+        <Services />
+      </div>
+
+      <div id="works">
+        <Works />
+      </div>
+
+      <div id="testimonials">
+        <Testimonial />
+      </div>
     </div>
   );
-};
+}
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,30 @@
 import React from "react";
-import { motion } from "framer-motion";
+
 const Navbar = () => {
+
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="absolute top-0 left-0 w-full p-12 flex justify-between items-center z-50 m-5 motion-preset-slide-down">
-      <div className="pointer-events-auto font-black  text-3xl ms-5">KERN.</div>
-      <ul className="flex  text-lg uppercase gap-10 font-semibold me-5">
-        <li className="cursor-pointer">About</li>
-        <li className="cursor-pointer">Works</li>
-        <li className="cursor-pointer">Services</li>
-        <li className="cursor-pointer">Contact</li>
+      <div 
+        className="pointer-events-auto font-black text-3xl ms-5 cursor-pointer"
+        onClick={() => scrollToSection("home")}
+      >
+        KERN.
+      </div>
+
+      <ul className="flex text-lg uppercase gap-10 font-semibold me-5">
+        <li className="cursor-pointer" onClick={() => scrollToSection("about")}>About</li>
+        <li className="cursor-pointer" onClick={() => scrollToSection("works")}>Works</li>
+        <li className="cursor-pointer" onClick={() => scrollToSection("services")}>Services</li>
+        <li className="cursor-pointer" onClick={() => scrollToSection("contact")}>Contact</li>
       </ul>
+
       <div className="font-bold text-xl underline underline-offset-8 hover:no-underline uppercase cursor-pointer mr-10 group relative">
         Let's connect
         <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-black transition-all duration-800 ease-out group-hover:w-full"/>
