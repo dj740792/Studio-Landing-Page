8958a0522d0237aa1f7cc98a1afcfe186178bea9
Dhruv <dj740792@gmail.com>
2026-05-04 12:13:22 +0530

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 const Hero = () => {
   return (
     <motion.div
-      className="relative h-screen  flex  justify-center top-[25%]"
+      className="relative h-screen  flex flex-col  items-center top-[15%]"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.8 }}
@@ -11,6 +11,23 @@ const Hero = () => {
       <h1 className="font-qurova md:text-[14rem] mt-12 scale-y-125 tracking-tighter leading-[1.2] whitespace-nowrap">
         <span>JULIAN GLASS </span>
       </h1>
+      <div className="flex justify-around items-center h-full w-full">
+        <div className="text-[6.5rem] md:text-[9rem] font-black leading-none">
+          20
+        </div>
+
+        <div className="flex flex-col items-center  gap-6 font-qurova">
+          <p className="text-xl">
+            ITS NEVER JUST A PHOTO. <br /> EVERY FRAME TELLS A STORY. <br />
+            WE CRAFT VISUAL IDENTITIES. <br />
+            YOUR BRAND. OUR LENS.
+          </p>
+        </div>
+
+        <div className="text-[6.5rem] md:text-[9rem] font-black leading-none">
+          26
+        </div>
+      </div>
     </motion.div>
   );
 };
